914565ec7fa0bbc500cdf8aeb1fb6a3d60534d28
Leonardo Campos <157067789+LeonardoCamposdev@users.noreply.github.com>
2025-07-29 19:27:41 -0300

    up

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// EFEITO MAQUINA DE DIGITAR
+function initTypeWriter() {
+  const titulo = document.querySelectorAll(".js-h1-efeito");
+
+  titulo.forEach((titulo) => {
+    function typeWriter(elemento) {
+      const textoArray = elemento.innerHTML.split(""); // o (split), separa cada letra do testo T,I,T,U,L,O
+      elemento.innerHTML = "";
+      textoArray.forEach((letra, i) => {
+        setTimeout(function () {
+          elemento.innerHTML += letra;
+        }, 130 * i);
+      });
+    }
+
+    typeWriter(titulo);
+  });
+}
+initTypeWriter();
+// EFEITO MAQUINA DE DIGITAR //
