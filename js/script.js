b701843ae54b9c8712cc51f5056d233e095d4e3a
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-08-02 17:32:42 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,20 +1,3 @@
-// EFEITO MAQUINA DE DIGITAR
-function initTypeWriter() {
-  const titulo = document.querySelectorAll(".js-h1-efeito");
+import initMaquinaDigitar from "./modules/efeito-maquina-digitar.js";
 
-  titulo.forEach((titulo) => {
-    function typeWriter(elemento) {
-      const textoArray = elemento.innerHTML.split(""); // o (split), separa cada letra do testo T,I,T,U,L,O
-      elemento.innerHTML = "";
-      textoArray.forEach((letra, i) => {
-        setTimeout(function () {
-          elemento.innerHTML += letra;
-        }, 130 * i);
-      });
-    }
-
-    typeWriter(titulo);
-  });
-}
-initTypeWriter();
-// EFEITO MAQUINA DE DIGITAR //
+initMaquinaDigitar();
