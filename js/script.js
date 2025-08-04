33c825e8c35f903d2f633c753988570286713740
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-08-04 12:40:20 -0300

    upp

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
 import initMaquinaDigitar from "./modules/efeito-maquina-digitar.js";
+import initAnimacaoScrollLeft from "./modules/animacao-scroll-left.js";
+
 
 initMaquinaDigitar();
+initAnimacaoScrollLeft();
